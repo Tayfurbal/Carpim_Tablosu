e12ac17d1c5e9de07995075b7eeb6810f8d9b2e8
Tayfunzat <tayfunzbal@gmail.com>
2021-07-23 14:29:30 +0300

    Çarpım Tablosu
    Bu program ile çarpım tablosunu oluşturabilirsiniz. Program C++ dilinde ve Rad Studio derleyicisi ile yazılmıştır.

---
+++ Codes.cpp
@@ -0,0 +1,20 @@
+#include <iostream.h>
+#include <conio.h>
+
+main ()
+{
+
+ for (int i=1; i<11;i++)
+  {
+	for (int j=1; j<11; j++)
+
+ {
+ cout<<i<< "*"<<j<<"="<<i*j<<endl;
+ }
+ cout<<endl;
+
+ }
+
+getch();
+}
+
